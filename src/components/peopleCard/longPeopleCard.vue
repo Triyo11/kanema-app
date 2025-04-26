0abedc43edd70a51a79f6b1bfb1aef2d4c7eac0c
Triyo Krismantoro <rusdiantoro117@gmail.com>
2025-04-26 15:43:04 +0700

    create PeopleCard component

---
+++ src/components/peopleCard/longPeopleCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { PhUser } from '@phosphor-icons/vue';
+
+const props = defineProps({
+  people: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div v-for="person in people" :key="person?.id" class="w-full h-fit flex items-center gap-2">
+    <div v-if="person?.profile_path" class="h-18 min-w-18 rounded-full overflow-hidden">
+      <img :src="`https://image.tmdb.org/t/p/w500${person?.profile_path}`" :alt="`${person?.name} Image`"
+        class="h-full w-full object-cover" />
+    </div>
+    <div v-else
+      class="h-18 min-w-18 rounded-full flex items-center justify-center bg-[var(--dark-green)] text-[var(--green)]]">
+      <PhUser :size="50" weight="fill" />
+    </div>
+    <div class="w-full flex flex-col">
+      <h3 class="text-xl text-[var(--green)] font-semibold">{{ person.name }}</h3>
+      <p class="text-lg">{{ person.character }}</p>
+    </div>
+  </div>
+</template>
---
+++ src/components/peopleCard/shortPeopleCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+const props = defineProps({
+  people: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div v-for="person in people" :key="person?.id" class="flex flex-col w-full max-h-fit items-center">
+    <img v-if="person?.profile_path" :src="`https://image.tmdb.org/t/p/w500${person?.profile_path}`" alt="Cast Image"
+      class="h-32 min-w-32 rounded-full object-cover">
+    <img v-else :src="`https://placehold.jp/097969/ffffff/350x350.png?text=%20`" alt="Cast Image"
+      class="h-32 min-w-32 rounded-full object-cover">
+    <p class="text-lg text-[var(--green)] font-semibold text-center">{{ person?.name }}</p>
+    <p class="text-lg text-[var(--white)] text-center">{{ person?.character }}</p>
+  </div>
+</template>
